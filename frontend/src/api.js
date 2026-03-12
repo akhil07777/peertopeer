f4b9c7c925e7abbb13b37178ccfe6450b2d2509f
Akhil Anand <akhilaanand107@gmail.com>
2026-03-12 19:53:18 +0530

    Set baseURL for API to localhost

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
- baseURL: "http://localhost:5000/api"//
-  //baseURL: "https://peertopeer-1-tx2n.onrender.com"//
+ baseURL: "http://localhost:5000/api"
+
 });
