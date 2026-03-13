7ece1ccede57c83b5dc8363c73183b6609a40a60
akhil a anand <akhilaanand107@gmail.com>
2026-03-13 22:36:16 +0530

    Update CORS configuration

--- server/server.js
+++ server/server.js
@@ -14,11 +14,10 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const app = express();
 
 // Middleware
+const cors = require("cors");
+
 app.use(cors({
-  origin: [
-    "http://localhost:3000", 
-    "https://peertopeer-qz72.vercel.app"   // change this to your frontend URL after deployment
-  ],
+  origin: true,
   credentials: true
 }));
 
